2950778fbae925b2da49aff3fb428701e2074254
abhi-kr07 <112303518+abhi-kr07@users.noreply.github.com>
2024-03-10 22:15:47 +0530

    check to list the github user having read access

---
+++ list_user.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Github API URL
+API_URL="https://api.github.com"
+
+#Github USERNAME and PERSONAL ACCESS TOKEN
+USERNAME=$username
+TOKEN=$token
+
+# User and Repo information
+#repo-owner=abhishek-yono , 
+REPO_OWNER=$1
+REPO_NAME=$2
+
+
+function github_api_get {
+    local endpoint="$1"
+    local url="${API_URL}/${endpoint}"
+
+    # send a get request to the Github API with authentication
+    curl -s -u "${USERNAME}:${TOKEN}" "$url"       
+}
+
+
+function list_user_with_read_access {
+    local endpoint="repos/${REPO_OWNER}/${REPO_NAME}/collaborators"
+    
+    #Fetch the list of user or collaborators having read access
+    collaborators="$(github_api_get "$endpoint")"
+
+    #print the list of user with read access 
+    if [ -z "$collaborators"]; 
+    then
+        echo "No user with read access found in ${REPO_OWNER}/${REPO_NAME}"
+    else
+        echo "User with read access to ${REPO_OWNER}/{REPO_NMAE}:"
+        echo "$collaborators"
+    fi
+}
+
+#Main Script
+
+echo "Listing users with read access to ${REPO_OWNER}/${REPO_NAME}.."
+list_user_with_read_access
