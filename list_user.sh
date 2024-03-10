4e3720b15288b15e39ac29757da9c54e40e6b5c7
abhi-kr07 <112303518+abhi-kr07@users.noreply.github.com>
2024-03-10 22:23:01 +0530

    Update list_user.sh

--- list_user.sh
+++ list_user.sh
@@ -26,7 +26,7 @@ function list_user_with_read_access {
     local endpoint="repos/${REPO_OWNER}/${REPO_NAME}/collaborators"
     
     #Fetch the list of user or collaborators having read access
-    collaborators="$(github_api_get "$endpoint")"
+    collaborators="$(github_api_get "$endpoint" | jq -r '.[] | select(.permission.pull == true) | .login')"
 
     #print the list of user with read access 
     if [[ -z "$collaborators"]]; then
